b8ca932f11437dc921a35c285046397af7bb80c9
theetom <tomazf4@gmail.com>
2026-01-28 16:12:07 +0000

    commit to merge to main

---
+++ inc/Client.hpp
@@ -0,0 +1,20 @@
+#ifndef CLIENT_HPP
+# define CLIENT_HPP
+
+#include <map>
+#include <string>
+
+class Client {
+	private:
+		std::map<std::string, bool> _channels //name client and whether is operator
+
+	public:
+		// constructors and destructors
+
+		// getters/setters
+
+
+
+};
+
+#endif
