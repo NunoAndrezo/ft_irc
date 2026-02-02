dc71057fdd44d4926c34f0655e97d76c9bbed0d4
theetom <tomazf4@gmail.com>
2026-02-02 13:33:15 +0000

    added command class

--- src/Commands/Command.cpp
+++ src/Commands/Command.cpp
@@ -6,8 +6,18 @@
 /*   By: toferrei <toferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 12:16:32 by toferrei          #+#    #+#             */
-/*   Updated: 2026/02/02 12:16:56 by toferrei         ###   ########.fr       */
+/*   Updated: 2026/02/02 13:32:51 by toferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Command.hpp"
+
+Command::Command(Server *server) : _server(server)
+{
+	
+}
+
+Command::~Command()
+{
+	
+}
--- src/Commands/Command.hpp
+++ src/Commands/Command.hpp
@@ -6,17 +6,23 @@
 /*   By: toferrei <toferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 12:16:35 by toferrei          #+#    #+#             */
-/*   Updated: 2026/02/02 12:16:48 by toferrei         ###   ########.fr       */
+/*   Updated: 2026/02/02 13:32:31 by toferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COMMAND_HPP
 # define COMMAND_HPP
 
+#include "../Server/Server.hpp"
+
+class Server;
+
 class Command
 {
+	protected:
+		Server *_server;
 	public:
-		Command();
+		Command(Server *server);
 		virtual ~Command();
 		
 		virtual void execute() = 0; // Pure virtual function to be overridden by derived classes
