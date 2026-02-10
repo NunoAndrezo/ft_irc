f09e5894eced3add24c5c932a4bd80a123611ba0
theetom <tomazf4@gmail.com>
2026-02-10 19:05:37 +0000

    kick done

--- src/Server/Server.cpp
+++ src/Server/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: toferrei <toferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 15:36:09 by toferrei          #+#    #+#             */
-/*   Updated: 2026/02/10 15:00:46 by toferrei         ###   ########.fr       */
+/*   Updated: 2026/02/10 15:32:23 by toferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,7 +75,6 @@ void Server::clientMessage(int i, Client &c)
 	}
 
 	c.appendBuffer(buf);
-	std::cout << ":"<< buf << ":" << c.getBuffer().empty() << std::endl;
 	size_t pos;
 	while (!c.getBuffer().empty())
 	{
